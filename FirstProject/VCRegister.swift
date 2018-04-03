368b63faaa28b73d0febd0d997c7102d712cedb3
nachog96 <fcta.1516.galani@gmail.com>
2018-04-03 14:47:01 +0200

    Segunda clase

---
+++ FirstProject/VCRegister.swift
@@ -0,0 +1,35 @@
+//
+//  VCRegister.swift
+//  FirstProject
+//
+//  Created by IGNACIO GALAN DE PINA on 3/4/18.
+//  Copyright © 2018 IGNACIO GALAN DE PINA. All rights reserved.
+//
+
+import UIKit
+
+class VCRegister: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
